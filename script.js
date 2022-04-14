aad5e836de99c1ef5e758b996679aee8d6f74075
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-04-14 13:21:43 -0400

    script shuffle

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+
+let song;
+let img;
+
+function preload() {
+    song = loadSound('assets/songs/lane8.mp3');
+    img = loadImage('https://images.unsplash.com/photo-1511489731872-324afc650052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80');
+
+}
+
+
+function setup(){
+    createCanvas(100, 100);
+    img.resize(width, height);
+    image(img, 0, 0);
+    noSmooth();
+}
+
+function playSong(){
+    song.play();
+}
+
+
 let touchX = 0;
 let touchY = 0;
 
@@ -52,22 +75,6 @@ ws.onmessage = (event) => {
 
 
 
-let song;
-let img;
-
-function preload() {
-    song = loadSound('assets/songs/lane8.mp3');
-    img = loadImage('https://images.unsplash.com/photo-1511489731872-324afc650052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80');
-
-}
-
-
-function setup(){
-    createCanvas(100, 100);
-    img.resize(width, height);
-    image(img, 0, 0);
-    noSmooth();
-}
 
 function draw(){
     img.loadPixels();
@@ -147,6 +154,3 @@ function getColorTotal(color) {
     return red(color) + green(color) + blue(color);
 }
 
-function playSong(){
-    song.play();
-}
